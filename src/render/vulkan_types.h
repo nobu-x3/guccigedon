f20eb3b43faf134cabfd33951d1171b4af66cb49
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-10-18 21:19:08 +0200

    Added VK_CHECK

--- src/render/vulkan_types.h
+++ src/render/vulkan_types.h
@@ -1,2 +1,13 @@
 #pragma once
 #include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+#include "../core/logger.h"
+
+#define VK_CHECK(x)                                                            \
+	do {                                                                       \
+		VkResult err = x;                                                      \
+		if (err) {                                                             \
+			core::Logger::Error("Detected Vulkan error: %s", err);             \
+			exit(-1);                                                          \
+		}                                                                      \
+	} while (0)
