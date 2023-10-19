5681623792705a63fd535998131f8a131c485106
Dominik <dominik_k@cherrypickgames.com>
2023-10-19 16:47:20 +0200

    More progress on pipeline builder

--- src/render/vulkan_builders.cpp
+++ src/render/vulkan_builders.cpp
@@ -1,4 +1,5 @@
 #include "vulkan_builders.h"
+#include "vulkan/vulkan_core.h"
 
 namespace vkbuild {
 
@@ -113,35 +114,103 @@ namespace vkbuild {
 	}
 
 	PipelineBuilder &
-	PipelineBuilder::set_vertex_input(VkPipelineVertexInputStateCreateInfo ci) {
-		_vertex_input = ci;
+	PipelineBuilder::add_vertex_binding(u32 stride,
+										VkVertexInputRate input_rate) {
+		VkVertexInputBindingDescription desc{
+			static_cast<u32>(_vertex_bindings.size()), stride, input_rate};
+		_vertex_bindings.push_back(desc);
+		return *this;
+	}
+
+	PipelineBuilder &PipelineBuilder::add_vertex_attribute(u32 binding,
+														   u32 location,
+														   VkFormat format,
+														   u32 offset) {
+		VkVertexInputAttributeDescription desc{location, binding, format,
+											   offset};
+		_vertex_attributes.push_back(desc);
 		return *this;
 	}
 
 	PipelineBuilder &PipelineBuilder::set_input_assembly(
-		VkPipelineInputAssemblyStateCreateInfo ci){
-			_input_assembly = ci;
-			return *this;
-		}
+		VkPrimitiveTopology topology,
+		VkPipelineInputAssemblyStateCreateFlags create_flags,
+		bool primitive_restart_enable) {
+		_input_assembly.topology = topology;
+		_input_assembly.flags = create_flags;
+		_input_assembly.primitiveRestartEnable = primitive_restart_enable;
+		return *this;
+	}
 
-	PipelineBuilder &PipelineBuilder::set_viewport(VkViewport viewport){
+	PipelineBuilder &PipelineBuilder::set_viewport(VkViewport viewport) {
 		_viewport = viewport;
 		return *this;
 	}
 
-	PipelineBuilder &PipelineBuilder::set_scissor(VkRect2D scissor);
+	PipelineBuilder &PipelineBuilder::set_scissor(VkRect2D scissor) {
+		_scissor = scissor;
+		return *this;
+	}
+
+	PipelineBuilder &PipelineBuilder::set_cull_mode(VkCullModeFlags cull_mode,
+													VkFrontFace front_face) {
+		_rasterizer.cullMode = cull_mode;
+		_rasterizer.frontFace = front_face;
+		return *this;
+	}
+
+	PipelineBuilder &PipelineBuilder::set_polygon_mode(VkPolygonMode mode) {
+		_rasterizer.polygonMode = mode;
+		return *this;
+	}
+
+	PipelineBuilder &PipelineBuilder::set_color_blend_enabled(bool val) {
+		_color_blend_attachment.blendEnable = val;
+		return *this;
+	}
 
 	PipelineBuilder &
-	PipelineBuilder::set_rasterizer(VkPipelineRasterizationStateCreateInfo ci);
+	PipelineBuilder::set_multisampling_enabled(bool val,
+											   VkSampleCountFlagBits count) {
+		_multisampling.sampleShadingEnable = val;
+		_multisampling.rasterizationSamples = count;
+		return *this;
+	}
 
 	PipelineBuilder &
-	PipelineBuilder::set_color_blend(VkPipelineColorBlendAttachmentState state);
+	PipelineBuilder::set_depth_testing(bool testEnabled, bool depthWrite,
+									   VkCompareOp compare_op) {
+		_depth_stencil.depthTestEnable = testEnabled;
+		_depth_stencil.depthWriteEnable = depthWrite;
+		_depth_stencil.depthCompareOp = compare_op;
+		return *this;
+	}
 
-	PipelineBuilder &
-	PipelineBuilder::set_multisampling(VkPipelineMultisampleStateCreateInfo ci);
+	PipelineBuilder &PipelineBuilder::set_color_blending_enabled(bool enabled,
+																 VkLogicOp op) {
+		_color_blend_state.logicOpEnable = enabled;
+		_color_blend_state.logicOp = op;
+		return *this;
+	}
 
-	VkPipelineLayout PipelineBuilder::build_layout();
+	PipelineBuilder &PipelineBuilder::add_dynamic_state(VkDynamicState dynamic_state){
+		_dynamic_states.push_back(dynamic_state);
+		_dynamic_state_cis.pDynamicStates = _dynamic_states.data();
+		_dynamic_state_cis.dynamicStateCount = static_cast<u32>(_dynamic_states.size());
+		return *this;
+	}
+
+	VkPipelineLayout PipelineBuilder::build_layout() {}
 
 	VkPipeline PipelineBuilder::build_pipeline(VkDevice device,
-											   VkRenderPass pass);
+											   VkRenderPass pass) {
+		VkPipelineVertexInputStateCreateInfo vertex_input = {
+			VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
+			nullptr,
+			0,
+			static_cast<u32>(_vertex_bindings.size()),
+			_vertex_bindings.data(),
+			static_cast<u32>(_vertex_attributes.size()),
+			_vertex_attributes.data()};
+	}
 } // namespace vkbuild
--- src/render/vulkan_builders.h
+++ src/render/vulkan_builders.h
@@ -5,7 +5,6 @@
 #include "../core/core.h"
 #include "vulkan_types.h"
 
-
 namespace vkbuild {
 	VkCommandPoolCreateInfo command_pool_ci(uint32_t q_fam_index,
 											VkCommandPoolCreateFlags flags = 0);
@@ -34,38 +33,122 @@ namespace vkbuild {
 
 	class PipelineBuilder {
 	private:
-		std::vector<VkPipelineShaderStageCreateInfo> _shader_stages;
-		VkPipelineVertexInputStateCreateInfo _vertex_input = {VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO, nullptr};
-		VkPipelineInputAssemblyStateCreateInfo _input_assembly = {VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO, nullptr};
+		std::vector<VkPipelineShaderStageCreateInfo> _shader_stages{};
+		array_list<VkVertexInputBindingDescription> _vertex_bindings{};
+		array_list<VkVertexInputAttributeDescription> _vertex_attributes{};
+
+		VkPipelineInputAssemblyStateCreateInfo _input_assembly{
+			VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+			nullptr, 0, VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST, false};
+
 		VkViewport _viewport = {};
 		VkRect2D _scissor = {};
-		VkPipelineRasterizationStateCreateInfo _rasterizer = {VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO, nullptr};
-		VkPipelineColorBlendAttachmentState _color_blend_attachment = {};
-		VkPipelineMultisampleStateCreateInfo _multisampling = {VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO, nullptr};
-		VkPipelineLayout _pipeline_layout {};
+
+		VkPipelineRasterizationStateCreateInfo _rasterizer{
+			VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+			nullptr,
+			0,
+			false,
+			false,
+			VK_POLYGON_MODE_FILL,
+			0,
+			VK_FRONT_FACE_CLOCKWISE,
+			false,
+			0.f,
+			0.f,
+			0.f,
+			0.f};
+
+		VkPipelineColorBlendAttachmentState _color_blend_attachment{
+			false,
+			VK_BLEND_FACTOR_SRC_ALPHA,
+			VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+			VK_BLEND_OP_ADD,
+			VK_BLEND_FACTOR_SRC_ALPHA,
+			VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
+			VK_BLEND_OP_ADD};
+
+		VkPipelineMultisampleStateCreateInfo _multisampling{
+			VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+			nullptr,
+			0,
+			VK_SAMPLE_COUNT_1_BIT,
+			false,
+			1.f,
+			nullptr,
+			0,
+			0};
+
+		VkPipelineDepthStencilStateCreateInfo _depth_stencil{
+			VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+			nullptr,
+			0,
+			false,
+			false,
+			VK_COMPARE_OP_LESS_OR_EQUAL,
+			false,
+			false,
+			{},
+			{},
+			0.f,
+			1.f};
+
+		VkPipelineColorBlendStateCreateInfo _color_blend_state{
+			// @TODO: add color attachments array_list
+			VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+			nullptr,
+			0,
+			false,
+			VK_LOGIC_OP_COPY,
+			1,
+			&_color_blend_attachment,
+			{0.f, 0.f, 0.f, 0.f}};
+
+		array_list<VkDynamicState> _dynamic_states{};
+
+		VkPipelineDynamicStateCreateInfo _dynamic_state_cis{
+			VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO, nullptr, 0, 0,
+			nullptr};
+
+		VkPipelineLayout _pipeline_layout{};
 
 	public:
 		PipelineBuilder &add_shader_stage(VkPipelineShaderStageCreateInfo ci);
-		
+
+		PipelineBuilder &add_vertex_binding(u32 stride,
+											VkVertexInputRate input_rate);
+
+		PipelineBuilder &add_vertex_attribute(u32 binding, u32 location,
+											  VkFormat format, u32 offset);
 
 		PipelineBuilder &
-		set_vertex_input(VkPipelineVertexInputStateCreateInfo ci);
+		set_input_assembly(VkPrimitiveTopology topology,
+						   VkPipelineInputAssemblyStateCreateFlags create_flags,
+						   bool primitive_restart_enable);
+
+		PipelineBuilder &set_cull_mode(VkCullModeFlags cull_mode,
+									   VkFrontFace front_face);
+
+		PipelineBuilder &set_polygon_mode(VkPolygonMode mode);
+
+		PipelineBuilder &set_default_color_blend_enabled(bool val);
+
+		PipelineBuilder &set_multisampling_enabled(
+			bool val, VkSampleCountFlagBits count = VK_SAMPLE_COUNT_1_BIT);
 
 		PipelineBuilder &
-		set_input_assembly(VkPipelineInputAssemblyStateCreateInfo ci);
+		set_depth_testing(bool testEnabled, bool depthWrite,
+						  VkCompareOp compare_op = VK_COMPARE_OP_LESS_OR_EQUAL);
+
+		PipelineBuilder &
+		set_color_blending_enabled(bool enabled,
+								   VkLogicOp op = VK_LOGIC_OP_COPY);
+
+		PipelineBuilder &add_dynamic_state(VkDynamicState dynamic_state);
 
 		PipelineBuilder &set_viewport(VkViewport viewport);
 
 		PipelineBuilder &set_scissor(VkRect2D scissor);
-
-		PipelineBuilder &
-		set_rasterizer(VkPipelineRasterizationStateCreateInfo ci);
-
-		PipelineBuilder &
-		set_color_blend(VkPipelineColorBlendAttachmentState state);
-
-		PipelineBuilder &
-		set_multisampling(VkPipelineMultisampleStateCreateInfo ci);
 
 		VkPipelineLayout build_layout();
 
