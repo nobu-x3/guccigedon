432a76dd9e6c6acc696e7073966b1117db358b12
Dominik Kurasbediani <dominik.kurasbediani@gmail.com>
2023-10-22 15:46:04 +0200

    Added hashing for materials

--- src/render/vulkan_types.h
+++ src/render/vulkan_types.h
@@ -3,9 +3,9 @@
 #include <cstddef>
 #include <cstring>
 #include <glm/glm.hpp>
+#include <vk_mem_alloc.h>
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan_core.h>
-#include <vk_mem_alloc.h>
 #include "../core/core.h"
 
 #define VK_CHECK(x)                                                            \
@@ -18,30 +18,27 @@
 	} while (0)
 
 struct Buffer {
-    VkBuffer handle {};
-    VmaAllocation memory {};
+	VkBuffer handle{};
+	VmaAllocation memory{};
 };
-
 
 struct Material {
-    VkPipeline pipeline {};
-    VkPipelineLayout layout {};
+	VkPipeline pipeline{};
+	VkPipelineLayout layout{};
 
-    bool operator==(const Material& other) const {
-        return pipeline == other.pipeline;
-    }
-
+	bool operator==(const Material& other) const {
+		return pipeline == other.pipeline;
+	}
 };
 
-    template<>
-    struct std::hash<Material>
-{
-    std::size_t operator()(const Material& k) const {
-        return hash<void*>()(k.pipeline);
-    }
+template <>
+struct std::hash<Material> {
+	std::size_t operator()(const Material& k) const {
+		return hash<void*>()(k.pipeline);
+	}
 };
 
 struct MeshPushConstant {
-    glm::vec4 data {};
-    glm::mat4 render_matrix {};
+	glm::vec4 data{};
+	glm::mat4 render_matrix{};
 };
