211e1abc0e62b8801378a11b40486555b17eb247
Dominik <dominik.kurasbediani@gmail.com>
2023-10-25 10:21:11 +0200

    Added sampler creation helpers

--- src/render/vulkan_builders.cpp
+++ src/render/vulkan_builders.cpp
@@ -177,6 +177,39 @@ namespace vkbuild {
 		return write;
 	}
 
+	VkSamplerCreateInfo sampler_create_info(
+		VkFilter filters,
+		VkSamplerAddressMode
+			samplerAddressMode) {
+		VkSamplerCreateInfo info = {};
+		info.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+		info.pNext = nullptr;
+
+		info.magFilter = filters;
+		info.minFilter = filters;
+		info.addressModeU = samplerAddressMode;
+		info.addressModeV = samplerAddressMode;
+		info.addressModeW = samplerAddressMode;
+
+		return info;
+	}
+
+	VkWriteDescriptorSet
+	write_descriptor_image(VkDescriptorType type, VkDescriptorSet dstSet,
+						   VkDescriptorImageInfo* imageInfo, uint32_t binding) {
+		VkWriteDescriptorSet write = {};
+		write.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+		write.pNext = nullptr;
+
+		write.dstBinding = binding;
+		write.dstSet = dstSet;
+		write.descriptorCount = 1;
+		write.descriptorType = type;
+		write.pImageInfo = imageInfo;
+
+		return write;
+	}
+
 	PipelineBuilder& PipelineBuilder::add_shader(VkDevice device,
 												 const char* path,
 												 ShaderType type) {
