8da8bf604145c3abe54db8228f7f483b1d8e9fb9
Dominik <dominik.kurasbediani@gmail.com>
2023-11-08 10:09:56 +0100

    Added experiments folder

---
+++ experiments/logging_exp.cpp
@@ -0,0 +1,14 @@
+#include "core/logger.h"
+
+int main() {
+	std::vector<std::jthread> threads;
+	for (int i = 0; i < 10; ++i) {
+		threads.emplace_back([i]() {
+			while (true) {
+				core::Logger::Trace("Hello from {}", i);
+			}
+		});
+	}
+
+	return 0;
+}
